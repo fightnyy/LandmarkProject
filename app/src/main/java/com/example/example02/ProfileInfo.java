ee55e886cd2247c981ce747c53d775ee55b2b41d
MINRYUL <rud0806@gmail.com>
2020-04-07 15:59:44 +0900

    update Profileinfo database

--- app/src/main/java/com/example/example02/ProfileInfo.java
+++ app/src/main/java/com/example/example02/ProfileInfo.java
@@ -3,10 +3,12 @@ package com.example.example02;
 public class ProfileInfo {
     private String name;
     private String address;
+    private String photoUrl;
 
-    public ProfileInfo(String name, String address) {
+    public ProfileInfo(String name, String address, String photoUrl) {
         this.name = name;
         this.address = address;
+        this.photoUrl = photoUrl;
     }
 
     public String getName() {
@@ -15,11 +17,13 @@ public class ProfileInfo {
 
     public void setName(String name) { this.name = name; }
 
-    public String getAddress(){
-        return this.address;
-    }
+    public String getAddress(){ return this.address; }
 
     public void setAddress(String address) {
         this.address = address;
     }
+
+    public String getPhotoUrl(){ return this.photoUrl; }
+
+    public void setPhotoUrl(String photoUrl){ this.photoUrl = photoUrl; }
 }
--- app/src/main/java/com/example/example02/ProfileSettingActivity.java
+++ app/src/main/java/com/example/example02/ProfileSettingActivity.java
@@ -3,31 +3,53 @@ package com.example.example02;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.UserProfileChangeRequest;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.TedPermission;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class ProfileSettingActivity extends AppCompatActivity {
     private static final String TAG = "ProfileSettingActivity";
+    private FirebaseUser user;
+
+    private Boolean isPermission = true;
+
+    private static final int PICK_FROM_ALBUM = 1; //onActivityResult 에서 requestCode 로 반환되는 값
+
+    private File tempFile; //받아온 이미지를 저장
+    private  Uri photoUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_setting);
+
+        tedPermission();    //앨범 권한 요청
 
         findViewById(R.id.done).setOnClickListener(onClickListener);
         findViewById(R.id.ProfileImageSetting).setOnClickListener(onClickListener);
@@ -39,6 +61,33 @@ public class ProfileSettingActivity extends AppCompatActivity {
         finish();
     }
 
+    private void tedPermission() {
+
+        PermissionListener permissionListener = new PermissionListener() {
+            @Override
+            public void onPermissionGranted() {
+                // 권한 요청 성공
+                isPermission = true;
+
+            }
+
+            @Override
+            public void onPermissionDenied(ArrayList<String> deniedPermissions) {
+                // 권한 요청 실패
+                isPermission = false;
+            }
+        };
+
+        TedPermission.with(this)
+                .setPermissionListener(permissionListener)
+                .setRationaleMessage(getResources().getString(R.string.permission_2))
+                .setDeniedMessage(getResources().getString(R.string.permission_1))
+                .setPermissions(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA)
+                .check();
+
+    }
+
+
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data){
         super .onActivityResult(requestCode, resultCode, data);
@@ -49,6 +98,45 @@ public class ProfileSettingActivity extends AppCompatActivity {
                 }
                 break;
             }
+        }
+
+        if (requestCode == PICK_FROM_ALBUM) {
+
+            photoUri = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), photoUri);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            Cursor cursor = null;
+
+            try {
+
+                /*
+                 *  Uri 스키마를
+                 *  content:/// 에서 file:/// 로  변경한다.
+                 */
+                String[] proj = {MediaStore.Images.Media.DATA};
+
+                assert photoUri != null;
+                cursor = getContentResolver().query(photoUri, proj, null, null, null);
+
+                assert cursor != null;
+                int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+
+                cursor.moveToFirst();
+
+                tempFile = new File(cursor.getString(column_index));
+
+            } finally {
+                if (cursor != null) {
+                    cursor.close();
+                }
+            }
+
+            setImage();
+
         }
     }
 
@@ -70,9 +158,12 @@ public class ProfileSettingActivity extends AppCompatActivity {
         String address = ((EditText) findViewById(R.id.addressEditText)).getText().toString();
 
         if (name.length() > 0 && address.length() > 0) {
-            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
             FirebaseFirestore db = FirebaseFirestore.getInstance();
-            ProfileInfo profileinfo = new ProfileInfo(name, address);
+            FirebaseStorage storage = FirebaseStorage.getInstance();
+            StorageReference storageRef = storage.getReference();
+            user = FirebaseAuth.getInstance().getCurrentUser();
+            final StorageReference mountainImagesRef = storageRef.child("users/" + user.getUid() + "/profileImage.jpg");
+            ProfileInfo profileinfo = new ProfileInfo(name, address, null);
 
             if(user != null) {
                 db.collection("users").document(user.getUid()).set(profileinfo)
@@ -95,10 +186,35 @@ public class ProfileSettingActivity extends AppCompatActivity {
         }
     }
 
+    private void setImage() {
+
+        ImageView imageView = findViewById(R.id.ProfileImageSetting);
+
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        Bitmap originalBm = BitmapFactory.decodeFile(tempFile.getAbsolutePath(), options);
+        Log.d(TAG, "setImage : " + tempFile.getAbsolutePath());
+
+        imageView.setImageBitmap(originalBm);
+
+        /**
+         *  tempFile 사용 후 null 처리.
+         *  (resultCode != RESULT_OK) 일 때 tempFile 을 삭제하기 때문에
+         *  기존에 데이터가 남아 있게 되면 원치 않은 삭제가 된다.
+         */
+        tempFile = null;
+
+    }
+
+    private void goToAlbum() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+        startActivityForResult(intent, PICK_FROM_ALBUM);
+    }
+
     private void startToast(String msg){ Toast.makeText(this, msg, Toast.LENGTH_SHORT).show(); }
 
     private void startSettingProfileImage() {
-        Intent intent = new Intent(this, CameraActivity.class);
-        startActivityForResult(intent, 0);
+        if(isPermission) goToAlbum();
+        else startToast("권한 사용에 동의하여 주세요.");
     }
 }
