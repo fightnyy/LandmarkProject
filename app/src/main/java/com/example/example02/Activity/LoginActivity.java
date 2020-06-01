715418cedc4d3649c9deb17953cb2ed11d69cacc
나영윤 <fightnyy@naver.com>
2020-06-01 16:33:07 +0900

    google login

--- app/src/main/java/com/example/example02/Activity/LoginActivity.java
+++ app/src/main/java/com/example/example02/Activity/LoginActivity.java
@@ -6,21 +6,35 @@ import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.example.example02.R;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.GoogleApi;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class LoginActivity extends BasisActivity {
+    private static final int RC_SIGN_IN = 10;
     private FirebaseAuth mAuth;
     ProgressDialog Dialog;
+    private GoogleSignInClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,16 +42,31 @@ public class LoginActivity extends BasisActivity {
         setContentView(R.layout.activity_login);
 
         // Initialize Firebase Auth
-        mAuth = FirebaseAuth.getInstance();
+
 
         findViewById(R.id.registerButton).setOnClickListener(onClickListener);
         findViewById(R.id.loginButton).setOnClickListener(onClickListener);
         findViewById(R.id.resetButton).setOnClickListener(onClickListener);
+        findViewById(R.id.googlelogin).setOnClickListener(onClickListener);
+
+        // [START config_signin]
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        // [END config_signin]
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        mAuth = FirebaseAuth.getInstance();
+
+
     }
 
     View.OnClickListener onClickListener = new View.OnClickListener() {
         public void onClick(View v) {
-            switch (v.getId()){
+            switch (v.getId()) {
                 case R.id.loginButton:
                     login();
                     break;
@@ -47,6 +76,10 @@ public class LoginActivity extends BasisActivity {
                 case R.id.resetButton:
                     myStartActivity(PasswordResetActivity.class);
                     break;
+                case R.id.googlelogin:
+                    Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+                    startActivityForResult(signInIntent, RC_SIGN_IN);
+
             }
         }
     };
@@ -54,7 +87,7 @@ public class LoginActivity extends BasisActivity {
     private void login() {
         String email = ((EditText) findViewById(R.id.emailText)).getText().toString();
         String password = ((EditText) findViewById(R.id.passwordText)).getText().toString();
-        
+
         if (email.length() > 0 && password.length() > 0) {
 
             mAuth.signInWithEmailAndPassword(email, password)
@@ -78,13 +111,71 @@ public class LoginActivity extends BasisActivity {
         }
     }
 
-    private void startToast(String msg){
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+                Log.d("LoginActivity","s1");
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                Log.d("LoginActivity","f1");
+                // [START_EXCLUDE]
+
+                // [END_EXCLUDE]
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+
+        // [START_EXCLUDE silent]
+
+        // [END_EXCLUDE]
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Log.d("LoginActivity","s1");
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+
+                            Log.d("LoginActivity","f1");
+                        }
+
+                        // [START_EXCLUDE]
+
+                        // [END_EXCLUDE]
+                    }
+                });
+    }
+    // [END auth_with_google]
+
+
+
+
+    private void startToast(String msg) {
         Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
 
-    private void myStartActivity(Class c){
+    private void myStartActivity(Class c) {
         Intent intent = new Intent(this, c);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(intent);
     }
+
+
 }
