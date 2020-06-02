c4d90beb0b52b49d245dacfd0822ac0d02f4bfcb
나영윤 <fightnyy@naver.com>
2020-06-02 22:46:49 +0900

    feed search success

--- app/src/main/java/com/example/example02/Activity/FeedActivity.java
+++ app/src/main/java/com/example/example02/Activity/FeedActivity.java
@@ -1,5 +1,7 @@
 package com.example.example02.Activity;
+
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -28,19 +30,22 @@ public class FeedActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private List<PostInfo> imageDTOs = new ArrayList<>();
     private FirebaseDatabase database;
+    private List<PostInfo> result = new ArrayList<>();
+    EditText editText;
     String search;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
         database = FirebaseDatabase.getInstance();
         final BoardRecyclerViewAdapter boardRecyclerViewAdapter = new BoardRecyclerViewAdapter();
-
+        Log.d("login","login");
         findViewById(R.id.btn_search).setOnClickListener(listener);
-        EditText editText=findViewById(R.id.et_search);
-        search=editText.getText().toString();
+        editText=findViewById(R.id.et_search);
 
-        GridLayoutManager layoutManager=new GridLayoutManager(this,3);
+
+        GridLayoutManager layoutManager = new GridLayoutManager(this, 3);
         recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setAdapter(boardRecyclerViewAdapter);
@@ -48,7 +53,7 @@ public class FeedActivity extends AppCompatActivity {
         database.getReference().child("posts").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-
+                Log.d("login","reach1"+dataSnapshot.toString());
                 imageDTOs.clear();
                 for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
                     PostInfo imageDTO = snapshot.getValue(PostInfo.class);
@@ -67,39 +72,41 @@ public class FeedActivity extends AppCompatActivity {
 
 
     }
+
     View.OnClickListener listener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            switch (v.getId())
-            {
+            switch (v.getId()) {
                 case R.id.btn_search:
+                    search = editText.getText().toString();
                     searchLocation(search);
+                    break;
             }
         }
     };
 
-    void searchLocation(String str)
-    {
-        database.getReference().child("posts").addValueEventListener(new ValueEventListener() {
+    void searchLocation(final String str) {
+        final BoardRecyclerViewAdapter boardRecyclerViewAdapter = new BoardRecyclerViewAdapter();
+        database.getReference().child("posts").orderByChild("location").equalTo(str).addValueEventListener(new ValueEventListener() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+            public void onDataChange(DataSnapshot dataSnapshot) {
 
-             imageDTOs.clear();
+                imageDTOs.clear();
                 for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                PostInfo imageDTO = snapshot.getValue(PostInfo.class);
-                imageDTOs.add(imageDTO);
+                    PostInfo imageDTO = snapshot.getValue(PostInfo.class);
+                    imageDTOs.add(imageDTO);
+                }
+                boardRecyclerViewAdapter.notifyDataSetChanged();
             }
 
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
 
-
-        }
-
-        @Override
-        public void onCancelled(DatabaseError databaseError) {
-
+            }
+        });
     }
-    });
-    }
+
+
     class BoardRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
         @Override
@@ -112,7 +119,7 @@ public class FeedActivity extends AppCompatActivity {
 
         @Override
         public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-            Glide.with(holder.itemView.getContext()).load(imageDTOs.get(position).getPhotoUrl()).into(((CustomViewHolder)holder).imageView);
+            Glide.with(holder.itemView.getContext()).load(imageDTOs.get(position).getPhotoUrl()).into(((CustomViewHolder) holder).imageView);
         }
 
         @Override
