78c19b5804c9ed5c166d24a3b9a6bd5621b6c6a7
Minryul <rud0806@gmail.com>
2020-06-08 03:02:15 +0900

    FollowInfo modify

--- app/src/main/java/com/example/example02/Info/FollowInfo.java
+++ app/src/main/java/com/example/example02/Info/FollowInfo.java
@@ -8,15 +8,10 @@ import java.util.Map;
 public class FollowInfo {
     public int followerCount = 0;
     public int followingCount = 0;
-    private String publisher;
     public Map<String, Boolean> follower = new HashMap<>();
     public Map<String, Boolean> following = new HashMap<>();
 
     public FollowInfo(){
-
-    }
-    public FollowInfo(String publisher){
-        this.publisher = publisher;
     }
 
     @Exclude
@@ -24,7 +19,6 @@ public class FollowInfo {
         HashMap<String, Object> result = new HashMap<>();
         result.put("followerCount", followerCount);
         result.put("followingCount", followingCount);
-        result.put("publisher", publisher);
         return result;
     }
 }
